e52b98d7df64ba7672e101f899ca1b499eecaeb3
Petie Rademeyer <70881506+froggy744@users.noreply.github.com>
2026-09-25 22:54:20 +0200

    Rebuild: carry photo fingerprints into gallery objects

--- src/photo.rs
+++ src/photo.rs
@@ -11,6 +11,8 @@ mod imp {
         pub path: RefCell<String>,
         pub favorite: Cell<bool>,
         pub rotation: Cell<i32>,
+        pub mtime: Cell<i64>,
+        pub size_bytes: Cell<i64>,
     }
 
     #[glib::object_subclass]
@@ -27,12 +29,21 @@ glib::wrapper! {
 }
 
 impl PhotoObject {
-    pub fn new(id: i64, path: impl Into<String>, favorite: bool, rotation: i32) -> Self {
+    pub fn new(
+        id: i64,
+        path: impl Into<String>,
+        favorite: bool,
+        rotation: i32,
+        mtime: Option<i64>,
+        size_bytes: Option<i64>,
+    ) -> Self {
         let object: Self = glib::Object::new();
         object.imp().id.set(id);
         object.imp().path.replace(path.into());
         object.imp().favorite.set(favorite);
         object.imp().rotation.set(rotation.rem_euclid(360));
+        object.imp().mtime.set(mtime.unwrap_or_default());
+        object.imp().size_bytes.set(size_bytes.unwrap_or_default());
         object
     }
 
@@ -58,5 +69,13 @@ impl PhotoObject {
 
     pub fn set_rotation(&self, rotation: i32) {
         self.imp().rotation.set(rotation.rem_euclid(360));
+    }
+
+    pub fn mtime(&self) -> i64 {
+        self.imp().mtime.get()
+    }
+
+    pub fn size_bytes(&self) -> i64 {
+        self.imp().size_bytes.get()
     }
 }
