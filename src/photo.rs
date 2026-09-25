55fcae26bccd63590f73e1f160199a5e96adf748
Petie Rademeyer <70881506+froggy744@users.noreply.github.com>
2026-09-25 16:29:07 +0200

    Prototype: add persistent favourite state to photo objects

--- src/photo.rs
+++ src/photo.rs
@@ -1,6 +1,6 @@
 use glib::subclass::prelude::*;
 use gtk4 as gtk;
-use std::cell::RefCell;
+use std::cell::{Cell, RefCell};
 
 mod imp {
     use super::*;
@@ -8,6 +8,7 @@ mod imp {
     #[derive(Default)]
     pub struct PhotoObject {
         pub path: RefCell<String>,
+        pub favorite: Cell<bool>,
     }
 
     #[glib::object_subclass]
@@ -24,13 +25,22 @@ glib::wrapper! {
 }
 
 impl PhotoObject {
-    pub fn new(path: impl Into<String>) -> Self {
+    pub fn new(path: impl Into<String>, favorite: bool) -> Self {
         let object: Self = glib::Object::new();
         object.imp().path.replace(path.into());
+        object.imp().favorite.set(favorite);
         object
     }
 
     pub fn path(&self) -> String {
         self.imp().path.borrow().clone()
+    }
+
+    pub fn favorite(&self) -> bool {
+        self.imp().favorite.get()
+    }
+
+    pub fn set_favorite(&self, favorite: bool) {
+        self.imp().favorite.set(favorite);
     }
 }
